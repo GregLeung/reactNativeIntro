a81816a4e2ba3a2057ad9b18f2000e366d1da307
GregLeung <km0205hk@gmail.com>
2019-11-19 22:07:30 +1100

    view image

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Image, View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+class Greeting extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    let pic ={
+      uri: 'https://cdn.imgbin.com/25/25/6/imgbin-cooked-rice-asian-cuisine-bowl-rice-xjAK5VsDfy4jRDCHHMxGxU6vi.jpg'
+    }
+
+
+    return (
+      <View style={{alignItems: 'center', top: 50, backgroundColor: 'yellow'}}>
+        <Image source={pic} style={{width:200, height: 100}}/>
+        <Greeting name='Hello little rice' />
+        <Greeting name='riceee' />
+        <Greeting name='rice jai' />
+      </View>
+    );
+  }
+}
