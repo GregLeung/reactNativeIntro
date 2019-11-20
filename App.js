c7e3b7f07a3643231b6a0e4783ed65882057e5d2
GregLeung <km0205hk@gmail.com>
2019-11-20 22:02:34 +1100

    state

--- App.js
+++ App.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react';
 import { Image, View, Text } from 'react-native';
 
+class Blink extends Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
+
+
+
 class Bananas extends Component {
   render() {
     let pic = {
@@ -23,7 +63,7 @@ class Greeting extends Component {
   }
 }
 
-export default class LotsOfGreetings extends Component {
+class LotsOfGreetings extends Component {
   render() {
     let pic ={
       uri: 'https://cdn.imgbin.com/25/25/6/imgbin-cooked-rice-asian-cuisine-bowl-rice-xjAK5VsDfy4jRDCHHMxGxU6vi.jpg'
